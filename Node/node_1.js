6cea46ccda03db1e25dae0367bb1f8a13cdeac00
rohannarava <santoshrohan.narava@gmail.com>
2019-08-28 15:51:28 +0530

    Add files via upload

---
+++ Node/node_1.js
@@ -0,0 +1,6 @@
+var protocol = require("http");
+protocol.createServer(function (request, response){
+    response.writeHead(200, {'Content-type' : 'text/html'});
+    response.end('Hello World');
+}
+).listen(4000);
